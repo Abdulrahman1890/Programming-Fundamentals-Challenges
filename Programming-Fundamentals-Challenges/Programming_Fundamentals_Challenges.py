7931c4d67f8798cefa375da2975ea90074a39c90
Abdulrahman1890 <abdulrahmanmm2011@hotmail.com>
2022-04-04 14:30:35 +0200

    Add project files.

---
+++ Programming-Fundamentals-Challenges/Programming_Fundamentals_Challenges.py
@@ -0,0 +1,94 @@
+#Chapter 3 - Lesson 7 Challenge
+
+print("Challenge 1:")
+
+# A message for the user
+message = "This is going to be tricky ;)"
+Message = "Very tricky!"
+print(message) # show the message on the screen
+
+# Perform mathematical operations
+result = 2**3
+print("2**3 =", result)
+result = 5 - 3
+#print("5 - 3 =", result)
+
+print("Challenge complete!")
+
+## The Output Will be =>  
+#Challenge 1:
+#This is going to be tricky ;)
+#2**3 = 8
+#Challenge complete!
+
+
+##################################################
+
+#Chapter 4 - Lesson 6 Challenge
+
+think = input("Guess what is my favorite food? ")
+
+if think == "Chocolate":
+    print("True its my favorite food")
+else:
+    print("No")
+
+print("Thanks")
+
+#################################################
+
+
+##Chapter 5 - Lesson 7 Challenge
+
+def favoriteCity(name):
+    print("My favorite city is", name)
+
+favoriteCity("Alexandria - Egypt")
+favoriteCity("Koala Lumbur - Malaysia")
+favoriteCity("Riyadh - Saudi Arabia")
+
+#################################################
+
+#Practice Exercises
+
+# 1. What’s the output of the following program?
+
+print("Practice Exercise 1:")
+
+# A message for the user
+message = "This is going to be tricky. ;)"
+Message = "Very tricky!"
+print(Message)
+
+# Perform mathematical operations
+result = 2**3
+print("2**3 =", result)
+result = 5 - 3
+print("5 - 3 =", result)
+
+## The Output Will be =>  
+
+#Practice Exercise 1:
+#Very tricky!
+#2**3 = 8
+#5 - 3 = 2
+
+# 2. What’s the output of the following program?
+
+print("Practice Exercise 2:")
+message = "Hope you’re having fun!"
+print("The message", message)
+
+# Perform mathematical operations
+answer = 5+2**3
+print("the answer is:", answer)
+result = 5 - 3
+#print(“5 - 3 =”, result)
+print("Done!")
+
+## The Output Will be =>  
+
+#Practice Exercise 2:
+#The message Hope you’re having fun!
+#the answer is: 13
+#Done!
